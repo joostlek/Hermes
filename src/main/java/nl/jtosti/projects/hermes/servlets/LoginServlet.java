f69daf9ca855a2a96f37edd64dbcbca2bee3e805
Joostlek <joostlek@outlook.com>
2018-05-17 16:23:39 +0200

    Added Locations

--- src/main/java/nl/jtosti/projects/hermes/servlets/LoginServlet.java
+++ src/main/java/nl/jtosti/projects/hermes/servlets/LoginServlet.java
@@ -1,6 +1,9 @@
 package nl.jtosti.projects.hermes.servlets;
 
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +15,34 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.addHeader("Access-Control-Allow-Origin", "*");
         BufferedReader bufferedReader = new BufferedReader(req.getReader());
-        System.out.println(bufferedReader.readLine());
-        resp.setStatus(201);
+        Gson gson = new Gson();
+        LoginRequest loginRequest = gson.fromJson(bufferedReader.readLine(), LoginRequest.class);
+        if (loginRequest.getPassword().equals("test")) {
+            resp.addCookie(new Cookie("session", req.getSession().getId()));
+            resp.setStatus(200);
+        } else {
+            resp.setStatus(404);
+        }
+    }
+
+    class LoginRequest {
+        private String email;
+        private String password;
+
+        public LoginRequest() {
+        }
+
+        public LoginRequest(String email, String password) {
+            this.email = email;
+            this.password = password;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        protected String getPassword() {
+            return password;
+        }
     }
 }
