6f282a14c0e3baa3909649fc2eb3a0430624c422
Joostlek <joostlek@outlook.com>
2018-05-19 15:09:20 +0200

    Added the add button the the types table

--- src/main/ngapp/src/app/types/types.module.ts
+++ src/main/ngapp/src/app/types/types.module.ts
@@ -4,15 +4,16 @@ import { CommonModule } from '@angular/common';
 import { TypesRoutingModule } from './types-routing.module';
 import { TypesComponent } from './types.component';
 import { TypeTableComponent } from './type-table/type-table.component';
-import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import {MatTableModule, MatPaginatorModule, MatSortModule, MatButtonModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    TypesRoutingModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatButtonModule,
+    TypesRoutingModule,
   ],
   declarations: [TypesComponent, TypeTableComponent]
 })
