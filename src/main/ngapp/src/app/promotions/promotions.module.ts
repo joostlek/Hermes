238c3d40e905756d2aa182e32141b3d1f3808fae
Joostlek <joostlek@outlook.com>
2018-05-20 14:31:46 +0200

    Added a more button to the Promotion table

--- src/main/ngapp/src/app/promotions/promotions.module.ts
+++ src/main/ngapp/src/app/promotions/promotions.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PromotionsRoutingModule } from './promotions-routing.module';
 import { PromotionsComponent } from './promotions.component';
 import { PromotionTableComponent } from './promotion-table/promotion-table.component';
-import {MatTableModule, MatPaginatorModule, MatSortModule, MatButtonModule} from '@angular/material';
+import {MatTableModule, MatPaginatorModule, MatSortModule, MatButtonModule, MatIconModule} from '@angular/material';
 import { PromotionDetailComponent } from './promotion-detail/promotion-detail.component';
 
 @NgModule({
@@ -14,6 +14,7 @@ import { PromotionDetailComponent } from './promotion-detail/promotion-detail.co
     MatPaginatorModule,
     MatSortModule,
     MatButtonModule,
+    MatIconModule,
     PromotionsRoutingModule,
   ],
   declarations: [PromotionsComponent, PromotionTableComponent, PromotionDetailComponent]
