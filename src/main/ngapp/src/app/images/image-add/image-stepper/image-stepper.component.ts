97bbd963077249d40e0677ecc0a905b9ff0f295c
Joostlek <joostlek@outlook.com>
2018-05-31 15:00:42 +0200

    Changed the image setup to use only one form

--- src/main/ngapp/src/app/images/image-add/image-stepper/image-stepper.component.ts
+++ src/main/ngapp/src/app/images/image-add/image-stepper/image-stepper.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {PromotionService} from "@app/_services/promotion.service";
 import {Promotion} from "@app/_models/promotion";
 import {ImageService} from "@app/_services/image.service";
+import {User} from "@app/_models/user";
 
 @Component({
   selector: 'app-image-stepper',
@@ -12,59 +13,46 @@ import {ImageService} from "@app/_services/image.service";
 export class ImageStepperComponent implements OnInit {
   isLinear = true;
   time = 0;
-  newPromotion = true;
-  promotions: Promotion[];
-  detailsFormGroup: FormGroup;
-  promotionFormGroup: FormGroup;
-  newPromotionFormGroup: FormGroup;
-  fileFormGroup: FormGroup;
-  newPromo: Promotion;
+  formGroup: FormGroup;
+  promotions: Promotion[] = [];
+  user: User;
 
+  get formArray(): AbstractControl | null { return this.formGroup.get('formArray'); }
 
-  constructor(private _formBuilder: FormBuilder, private promotionService: PromotionService, private imageService: ImageService) { }
+  constructor(private _formBuilder: FormBuilder,
+              private promotionService: PromotionService,
+              private imageService: ImageService) {
+    this.user = JSON.parse(localStorage.getItem('user'));
+  }
 
   ngOnInit() {
-    this.getPromotions();
-    this.detailsFormGroup = this._formBuilder.group({
-      imageName: ['', Validators.required],
-      time_slider: [0, Validators.required]
+    this.formGroup = this._formBuilder.group({
+      formArray: this._formBuilder.array([
+        this._formBuilder.group({
+          imageName: ['', Validators.required],
+          timeSlider: [0, Validators.required],
+          selectPromotion: ['', Validators.required],
+        }),
+        this._formBuilder.group({
+          file: ['', Validators.required],
+        })
+      ])
     });
-    this.promotionFormGroup = this._formBuilder.group({
-      select_promotion: ['', []]
-    });
-    this.newPromotionFormGroup = this._formBuilder.group({
-      promotionName: ['', Validators.required]
-    });
-    this.fileFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-  }
-
-  getPromotions(): void {
-    this.promotionService.getPromotions()
-      .subscribe(promotions => this.promotions = promotions);
-  }
-
-  check_promotion(event) {
-    this.newPromotion = event === undefined;
   }
 
   update_time(event) {
     this.time = event;
   }
 
-  create_promotion(): void {
-    // this.newPromo = this.promotionService.addPromotion(this.newPromotionFormGroup.value['promotionName']);
-  }
-
   finishSetUp() {
-    let promo;
-    if (this.newPromotion) {
-      promo = this.newPromo;
-    } else {
-      promo = this.promotions[this.promotionFormGroup.value['select_promotion'] - 1]
-    }
-    this.imageService.addImage(this.detailsFormGroup.value['imageName'], promo, 1, 1080, 1920, ' ', this.time);
+    this.imageService.addImage(
+      this.formArray.get([0]).value['imageName'],
+      this.formArray.get([0]).value['selectPromotion'],
+      this.user.id,
+      1080,
+      1920,
+      ' ',
+      this.formArray.get([0]).value['timeSlider']);
   }
 }
 
