dd67206aaf29a3934d2caa029732916f6d0ab638
Joostlek <joostlek@outlook.com>
2018-06-17 15:57:50 +0200

    ONE LINERS... FUCK YEA!

--- src/main/java/nl/jtosti/projects/hermes/resources/UserResource.java
+++ src/main/java/nl/jtosti/projects/hermes/resources/UserResource.java
@@ -1,9 +1,9 @@
 package nl.jtosti.projects.hermes.resources;
 
-import com.google.gson.Gson;
-import nl.jtosti.projects.hermes.persistence.UserManager;
 import nl.jtosti.projects.hermes.models.User;
+import nl.jtosti.projects.hermes.persistence.ManagerProvider;
 import nl.jtosti.projects.hermes.responses.UserResponse;
+import nl.jtosti.projects.hermes.util.GsonProvider;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -15,45 +15,35 @@ public class UserResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getUsers() {
-        Gson gson = new Gson();
-        UserManager userManager = new UserManager();
         List<UserResponse> userResponses = new ArrayList<>();
-        for (User user: userManager.getAll()) {
+        for (User user: ManagerProvider.getUserManager().getAll()) {
             userResponses.add(user.toResponse());
         }
-        return gson.toJson(userResponses);
+        return GsonProvider.getGson().toJson(userResponses);
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getUser(@PathParam("id") int id) {
-        Gson gson = new Gson();
-        UserManager userManager = new UserManager();
-        return gson.toJson(userManager.get(id).toResponse());
+        return GsonProvider.getGson().toJson(ManagerProvider.getUserManager().get(id).toResponse());
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public String addUser(String body) {
-        Gson gson = new Gson();
-        UserManager userManager = new UserManager();
-        return gson.toJson(userManager.save(gson.fromJson(body, User.class)).toResponse());
+        return GsonProvider.getGson().toJson(ManagerProvider.getUserManager().save(GsonProvider.getGson().fromJson(body, User.class)).toResponse());
     }
 
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     public String updateUser(String body) {
-        Gson gson = new Gson();
-        UserManager userManager = new UserManager();
-        return gson.toJson(userManager.update(gson.fromJson(body, User.class)).toResponse());
+        return GsonProvider.getGson().toJson(ManagerProvider.getUserManager().update(GsonProvider.getGson().fromJson(body, User.class)).toResponse());
     }
 
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
     public String deleteUser(String body) {
-        Gson gson = new Gson();
-        UserManager userManager = new UserManager();
-        return gson.toJson(userManager.delete(gson.fromJson(body, User.class)));
+        return GsonProvider.getGson().toJson(ManagerProvider.getUserManager().delete(GsonProvider.getGson().fromJson(body, User.class)));
     }
 }
