1abdb3eb74611a7130018a8f4711acf72af5bf37
Joostlek <joostlek@outlook.com>
2018-06-17 16:13:14 +0200

    Finished ImageResource

--- src/main/java/nl/jtosti/projects/hermes/resources/ImageResource.java
+++ src/main/java/nl/jtosti/projects/hermes/resources/ImageResource.java
@@ -1,8 +1,8 @@
 package nl.jtosti.projects.hermes.resources;
 
-import com.google.gson.Gson;
-import nl.jtosti.projects.hermes.persistence.ImageManager;
 import nl.jtosti.projects.hermes.models.Image;
+import nl.jtosti.projects.hermes.persistence.ManagerProvider;
+import nl.jtosti.projects.hermes.util.GsonProvider;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -12,41 +12,31 @@ public class ImageResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getImages() {
-        Gson gson = new Gson();
-        ImageManager imageManager = new ImageManager();
-        return gson.toJson(imageManager.getAll());
+        return GsonProvider.getGson().toJson(ManagerProvider.getImageManager().getAll());
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getImage(@PathParam("id") int id) {
-        Gson gson = new Gson();
-        ImageManager imageManager = new ImageManager();
-        return gson.toJson(imageManager.get(id));
+        return GsonProvider.getGson().toJson(ManagerProvider.getImageManager().get(id));
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public String addImage(String body) {
-        Gson gson = new Gson();
-        ImageManager imageManager = new ImageManager();
-        return gson.toJson(imageManager.save(gson.fromJson(body, Image.class)));
+        return GsonProvider.getGson().toJson(ManagerProvider.getImageManager().save(GsonProvider.getGson().fromJson(body, Image.class)));
     }
 
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     public String updateImage(String body) {
-        Gson gson = new Gson();
-        ImageManager imageManager = new ImageManager();
-        return gson.toJson(imageManager.update(gson.fromJson(body, Image.class)));
+        return GsonProvider.getGson().toJson(ManagerProvider.getImageManager().update(GsonProvider.getGson().fromJson(body, Image.class)));
     }
 
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
     public String deleteImage(String body) {
-        Gson gson = new Gson();
-        ImageManager imageManager = new ImageManager();
-        return gson.toJson(imageManager.delete(gson.fromJson(body, Image.class)));
+        return GsonProvider.getGson().toJson(ManagerProvider.getImageManager().delete(GsonProvider.getGson().fromJson(body, Image.class)));
     }
 }
