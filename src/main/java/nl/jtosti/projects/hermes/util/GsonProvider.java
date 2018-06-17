f1926c1f14696efed993c63387117ec3b9e34cd8
Joostlek <joostlek@outlook.com>
2018-06-17 15:53:18 +0200

    Added Gson provider

---
+++ src/main/java/nl/jtosti/projects/hermes/util/GsonProvider.java
@@ -0,0 +1,13 @@
+package nl.jtosti.projects.hermes.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonProvider {
+    private static Gson gson = new GsonBuilder()
+            .create();
+
+    public static Gson getGson() {
+        return gson;
+    }
+}
