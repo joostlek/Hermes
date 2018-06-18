4eaffd63eb6f83836621f1a6e21b2b5bcb6a48e5
Joostlek <joostlek@outlook.com>
2018-06-18 21:50:52 +0200

    Changed the String to Response

--- src/main/java/nl/jtosti/projects/hermes/resources/TypeResource.java
+++ src/main/java/nl/jtosti/projects/hermes/resources/TypeResource.java
@@ -7,6 +7,7 @@ import nl.jtosti.projects.hermes.util.GsonProvider;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,36 +15,46 @@ import java.util.List;
 public class TypeResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getTypes() {
+    public Response getTypes() {
         List<TypeResponse> typeResponses = new ArrayList<>();
         for (Type type: ManagerProvider.getTypeManager().getAll()) {
             typeResponses.add(type.toResponse());
         }
-        return GsonProvider.getGson().toJson(typeResponses);
+        return Response
+                .ok(GsonProvider.getGson().toJson(typeResponses))
+                .build();
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getType(@PathParam("id") int id) {
-        return GsonProvider.getGson().toJson(ManagerProvider.getTypeManager().get(id).toResponse());
+    public Response getType(@PathParam("id") int id) {
+        return Response
+                .ok(GsonProvider.getGson().toJson(ManagerProvider.getTypeManager().get(id).toResponse()))
+                .build();
     }
 
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
-    public String updateType(String body) {
-        return GsonProvider.getGson().toJson(ManagerProvider.getTypeManager().update(GsonProvider.getGson().fromJson(body, Type.class)).toResponse());
+    public Response updateType(String body) {
+        return Response
+                .ok(GsonProvider.getGson().toJson(ManagerProvider.getTypeManager().update(GsonProvider.getGson().fromJson(body, Type.class)).toResponse()))
+                .build();
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public String addType(String body) {
-        return GsonProvider.getGson().toJson(ManagerProvider.getTypeManager().save(GsonProvider.getGson().fromJson(body, Type.class)).toResponse());
+    public Response addType(String body) {
+        return Response
+                .ok(GsonProvider.getGson().toJson(ManagerProvider.getTypeManager().save(GsonProvider.getGson().fromJson(body, Type.class)).toResponse()))
+                .build();
     }
 
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
-    public String deleteType(String body) {
-        return GsonProvider.getGson().toJson(ManagerProvider.getTypeManager().save(GsonProvider.getGson().fromJson(body, Type.class)));
+    public Response deleteType(String body) {
+        return Response
+                .ok(GsonProvider.getGson().toJson(ManagerProvider.getTypeManager().save(GsonProvider.getGson().fromJson(body, Type.class))))
+                .build();
     }
 }
