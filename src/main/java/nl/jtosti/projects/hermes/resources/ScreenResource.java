38b233a5a331b639ba23d255bc83ec9a391a4202
Joostlek <joostlek@outlook.com>
2018-06-18 21:49:00 +0200

    Changed the String to Response

--- src/main/java/nl/jtosti/projects/hermes/resources/ScreenResource.java
+++ src/main/java/nl/jtosti/projects/hermes/resources/ScreenResource.java
@@ -7,6 +7,7 @@ import nl.jtosti.projects.hermes.util.GsonProvider;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,36 +15,46 @@ import java.util.List;
 public class ScreenResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getScreens() {
+    public Response getScreens() {
         List<ScreenResponse> screenResponses = new ArrayList<>();
         for (Screen screen: ManagerProvider.getScreenManager().getAll()) {
             screenResponses.add(screen.toResponse());
         }
-        return GsonProvider.getGson().toJson(screenResponses);
+        return Response
+                .ok(GsonProvider.getGson().toJson(screenResponses))
+                .build();
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getScreen(@PathParam("id") int id) {
-        return GsonProvider.getGson().toJson(ManagerProvider.getScreenManager().get(id).toResponse());
+    public Response getScreen(@PathParam("id") int id) {
+        return Response
+                .ok(GsonProvider.getGson().toJson(ManagerProvider.getScreenManager().get(id).toResponse()))
+                .build();
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public String addScreen(String body) {
-        return GsonProvider.getGson().toJson(ManagerProvider.getScreenManager().save(GsonProvider.getGson().fromJson(body, Screen.class)).toResponse());
+    public Response addScreen(String body) {
+        return Response
+                .ok(GsonProvider.getGson().toJson(ManagerProvider.getScreenManager().save(GsonProvider.getGson().fromJson(body, Screen.class)).toResponse()))
+                .build();
     }
 
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
-    public String updateScreen(String body) {
-        return GsonProvider.getGson().toJson(ManagerProvider.getScreenManager().update(GsonProvider.getGson().fromJson(body, Screen.class)).toResponse());
+    public Response updateScreen(String body) {
+        return Response
+                .ok(GsonProvider.getGson().toJson(ManagerProvider.getScreenManager().update(GsonProvider.getGson().fromJson(body, Screen.class)).toResponse()))
+                .build();
     }
 
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
-    public String deleteScreen(String body) {
-        return GsonProvider.getGson().toJson(ManagerProvider.getScreenManager().delete(GsonProvider.getGson().fromJson(body, Screen.class)));
+    public Response deleteScreen(String body) {
+        return Response
+                .ok(GsonProvider.getGson().toJson(ManagerProvider.getScreenManager().delete(GsonProvider.getGson().fromJson(body, Screen.class))))
+                .build();
     }
 }
