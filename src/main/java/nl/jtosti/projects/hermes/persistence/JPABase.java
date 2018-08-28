bfcf6f72bf448888f2ab8ab70f89734c20929587
joostlek <joostlek@outlook.com>
2018-08-28 13:38:58 +0200

    Use global envs to access the database

--- src/main/java/nl/jtosti/projects/hermes/persistence/JPABase.java
+++ src/main/java/nl/jtosti/projects/hermes/persistence/JPABase.java
@@ -4,14 +4,33 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.FlushModeType;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JPABase {
     protected final EntityManager getConnection() {
         EntityManager em;
         String cfg = "postgresql";
         EntityManagerFactory entityManagerFactory;
+        Map<String, String> env = System.getenv();
+        Map<String, Object> configOverrides = new HashMap<>();
+        for (String envName : env.keySet()) {
+            if (envName.contains("DB_USER")) {
+                configOverrides.put("javax.persistence.jdbc.user", env.get(envName));
+            } else if (envName.contains("DB_PASSWORD")) {
+                configOverrides.put("javax.persistence.jdbc.password", env.get(envName));
+            } else if (envName.contains("DB_URL")) {
+                configOverrides.put("javax.persistence.jdbc.url", env.get(envName));
+            } else if (envName.contains("DB_DRIVER")) {
+                configOverrides.put("javax.persistence.jdbc.driver", env.get(envName));
+            } else if (envName.contains("DB_DIALECT")) {
+                configOverrides.put("hibernate.dialect", env.get(envName));
+            } else if (envName.contains("db_SHOW_SQL")) {
+                configOverrides.put("hibernate.show_sql", env.get(envName));
+            }
+        }
         try {
-            entityManagerFactory = Persistence.createEntityManagerFactory(cfg);
+            entityManagerFactory = Persistence.createEntityManagerFactory(cfg, configOverrides);
             em = entityManagerFactory.createEntityManager();
             em.setFlushMode(FlushModeType.AUTO);
         } catch (Throwable ex) {
