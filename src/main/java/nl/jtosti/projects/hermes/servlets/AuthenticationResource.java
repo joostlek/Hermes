7f1d486830d34379801cc105f88bd2ca0b15da6e
joostlek <joostlek@outlook.com>
2018-09-03 10:29:35 +0200

    Remove obsolete code

--- src/main/java/nl/jtosti/projects/hermes/servlets/AuthenticationResource.java
+++
@@ -1,9 +0,0 @@
-package nl.jtosti.projects.hermes.servlets;
-
-public class AuthenticationResource {
-    public static final String ROLE_OWNER_AD = "OwnerAd";
-    public static final String ROLE_SUPERUSER = "Superuser";
-    public static final String ROLE_ADVERTISING = "Advertiser";
-    public static final String ROLE_OWNER = "Owner";
-    public static final String ROLE_USER = "User";
-}
