9f448a6623a6b293a251182a4bb1c7dafe35302c
joostlek <joostlek@outlook.com>
2018-09-04 00:02:41 +0200

    Make Util class nicer

--- src/main/java/nl/jtosti/projects/hermes/util/Util.java
+++ src/main/java/nl/jtosti/projects/hermes/util/Util.java
@@ -1,16 +1,16 @@
 package nl.jtosti.projects.hermes.util;
 
 public class Util {
-    public static String MD5(String md5) {
+    private static String MD5(String md5) {
         try {
             java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
             byte[] array = md.digest(md5.getBytes());
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < array.length; ++i) {
-                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+            StringBuilder sb = new StringBuilder();
+            for (byte anArray : array) {
+                sb.append(Integer.toHexString((anArray & 0xFF) | 0x100), 1, 3);
             }
             return sb.toString();
-        } catch (java.security.NoSuchAlgorithmException e) {
+        } catch (java.security.NoSuchAlgorithmException ignored) {
         }
         return null;
     }
