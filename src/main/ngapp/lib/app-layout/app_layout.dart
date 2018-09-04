293904864cf2a73cedc5dae57c901532f435752d
joostlek <joostlek@outlook.com>
2018-09-05 01:17:02 +0200

    Add AngularDart for frontend

---
+++ src/main/ngapp/lib/app-layout/app_layout.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+    selector: 'app-layout',
+    templateUrl: 'app_layout.html',
+    styleUrls: [
+      'package:angular_components/app_layout/layout.scss.css',
+      'app_layout.scss.css'
+    ])
+class MyComponent {}
