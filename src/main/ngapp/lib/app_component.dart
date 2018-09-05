292c9ec12c3b455a41bcc1ce99556dd8b95a5efd
joostlek <joostlek@outlook.com>
2018-09-05 14:06:10 +0200

    Meh, normal Angular way to go

--- src/main/ngapp/lib/app_component.dart
+++
@@ -1,9 +0,0 @@
-import 'package:angular/angular.dart';
-
-@Component(
-  selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1><app-layout></app-layout>',
-)
-class AppComponent {
-  var name = 'Angular';
-}
