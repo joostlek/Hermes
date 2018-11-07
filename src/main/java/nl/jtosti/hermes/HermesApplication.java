844a757e79e50a7b5e0915d1265c04a4c7bf54ec
joostlek <joostlek@outlook.com>
2018-11-08 00:45:51 +0100

    Remove example code

--- src/main/java/nl/jtosti/hermes/HermesApplication.java
+++ src/main/java/nl/jtosti/hermes/HermesApplication.java
@@ -1,15 +1,10 @@
 package nl.jtosti.hermes;
 
-import javafx.application.Application;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HermesApplication {
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(HermesApplication.class, args);
 	}
