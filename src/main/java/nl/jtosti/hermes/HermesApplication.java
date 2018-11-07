5225812315fff02650aa3daa2497b2f925d39148
joostlek <joostlek@outlook.com>
2018-11-08 00:45:16 +0100

    Remove example code

--- src/main/java/nl/jtosti/hermes/HermesApplication.java
+++ src/main/java/nl/jtosti/hermes/HermesApplication.java
@@ -1,14 +1,10 @@
 package nl.jtosti.hermes;
 
 import javafx.application.Application;
-import nl.jtosti.hermes.entities.User;
-import nl.jtosti.hermes.repositories.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HermesApplication {
@@ -17,17 +13,4 @@ public class HermesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HermesApplication.class, args);
 	}
-
-	@Bean
-    public CommandLineRunner demo(UserRepository repository) {
-	    return (args) -> {
-	        repository.save(new User("Joost", "Lekkerkerker"));
-	        for (User user : repository.findAll()) {
-	            log.info(user.toString());
-            }
-            for (User user : repository.findByLastName("Lekkerkerker")) {
-                log.info(user.toString());
-            }
-        };
-    }
 }
