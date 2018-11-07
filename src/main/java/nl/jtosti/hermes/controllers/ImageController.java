0dd84186aa7fd9908a06068233966890271b2f10
joostlek <joostlek@outlook.com>
2018-11-08 00:53:05 +0100

    Add restcontrollers for each entity

---
+++ src/main/java/nl/jtosti/hermes/controllers/ImageController.java
@@ -0,0 +1,11 @@
+package nl.jtosti.hermes.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImageController {
+    private final Logger log = LoggerFactory.getLogger(ImageController.class);
+
+}
---
+++ src/main/java/nl/jtosti/hermes/controllers/LocationController.java
@@ -0,0 +1,10 @@
+package nl.jtosti.hermes.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocationController {
+    private final Logger log = LoggerFactory.getLogger(ImageController.class);
+}
