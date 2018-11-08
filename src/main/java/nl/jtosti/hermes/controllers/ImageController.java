8cbefeb2aa7a0fa3074a2766aa1f7b2828d1de79
joostlek <joostlek@outlook.com>
2018-11-08 02:03:41 +0100

    Remove loggers because they're not needed yet

--- src/main/java/nl/jtosti/hermes/controllers/ImageController.java
+++ src/main/java/nl/jtosti/hermes/controllers/ImageController.java
@@ -1,11 +1,8 @@
 package nl.jtosti.hermes.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageController {
-    private final Logger log = LoggerFactory.getLogger(ImageController.class);
 
 }
