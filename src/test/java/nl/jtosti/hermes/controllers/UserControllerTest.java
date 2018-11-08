73289a3f9f7a7b898a43be85f2c64d2f952af233
joostlek <joostlek@outlook.com>
2018-11-08 02:20:39 +0100

    Create test for controller

---
+++ src/test/java/nl/jtosti/hermes/controllers/UserControllerTest.java
@@ -0,0 +1,48 @@
+package nl.jtosti.hermes.controllers;
+
+import nl.jtosti.hermes.entities.User;
+import nl.jtosti.hermes.services.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserService service;
+
+    @Test
+    public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
+        User user = new User("Alex", "Jones", "alex.jones@alex.com");
+        List<User> allUsers = Collections.singletonList(user);
+        given(service.getAllUsers()).willReturn(allUsers);
+
+        mvc.perform(get("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(user("user")))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].firstName", is(user.getFirstName())));
+    }
+
+}
