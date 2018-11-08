aa09d75fce8d32a5f73d640c289ee8a40c6692b7
joostlek <joostlek@outlook.com>
2018-11-08 01:04:36 +0100

    Add tests for repositories

---
+++ src/test/java/nl/jtosti/hermes/repositories/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package nl.jtosti.hermes.repositories;
+
+import nl.jtosti.hermes.entities.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void findByLastName() {
+        User user = new User("Alex", "Jones");
+        entityManager.persist(user);
+        entityManager.flush();
+
+        User found = userRepository.findByLastName(user.getLastName()).get(0);
+
+        assertThat(found.getFirstName()).isEqualTo(user.getFirstName());
+    }
+
+
+}
