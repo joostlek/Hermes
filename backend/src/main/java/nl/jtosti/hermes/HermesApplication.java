d1c007458b1a6955fe368e3e72d9c4d4087e62fd
joostlek <joostlek@outlook.com>
2018-11-25 17:38:34 +0100

    Disable Spring Security

--- backend/src/main/java/nl/jtosti/hermes/HermesApplication.java
+++ backend/src/main/java/nl/jtosti/hermes/HermesApplication.java
@@ -2,11 +2,12 @@ package nl.jtosti.hermes;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class HermesApplication {
     public static void main(String[] args) {
         SpringApplication.run(HermesApplication.class, args);
