94b4b6d0cc81df020f43a61d7129ca803aff946c
joostlek <joostlek@outlook.com>
2019-01-08 22:08:46 +0100

    Remove HttpClientModule import, so the interceptor works

--- frontend/src/app/@core/data/data.module.ts
+++ frontend/src/app/@core/data/data.module.ts
@@ -1,5 +1,4 @@
 import {CommonModule} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {ImageService} from './image.service';
 import {LocationService} from './location.service';
@@ -10,7 +9,6 @@ import {SelectorService} from './selector.service';
     declarations: [],
     imports: [
         CommonModule,
-        HttpClientModule,
     ],
     providers: [
         LocationService,
