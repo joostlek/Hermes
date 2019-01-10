cef04d4ab38345e74079179a72d3f4af3fa65815
joostlek <joostlek@outlook.com>
2019-01-10 13:02:42 +0100

    Change AuthenticationRequest to be an interface, so Screens can send their ID as int and not as String

--- backend/src/main/java/nl/jtosti/hermes/security/AuthenticationRequest.java
+++ backend/src/main/java/nl/jtosti/hermes/security/AuthenticationRequest.java
@@ -1,24 +1,7 @@
 package nl.jtosti.hermes.security;
 
-import java.io.Serializable;
+public interface AuthenticationRequest {
+    String getUsername();
 
-public class AuthenticationRequest implements Serializable {
-    private String username;
-    private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    String getPassword();
 }
---
+++ backend/src/main/java/nl/jtosti/hermes/security/ScreenAuthenticationRequest.java
@@ -0,0 +1,26 @@
+package nl.jtosti.hermes.security;
+
+import java.io.Serializable;
+
+public class ScreenAuthenticationRequest implements Serializable, AuthenticationRequest {
+    private Long screenId;
+    private String password;
+
+    public void setScreenId(Long screenId) {
+        this.screenId = screenId;
+    }
+
+    @Override
+    public String getUsername() {
+        return Long.toString(screenId);
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+}
