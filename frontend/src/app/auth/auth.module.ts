ddc880fd6e9a4c148631a7fdfdfe983817fa285d
joostlek <joostlek@outlook.com>
2019-01-13 15:44:17 +0100

    Export the wrapper to the auth component Create register module
    Signed-off-by: joostlek <joostlek@outlook.com>

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -9,9 +9,10 @@ import {AuthComponent} from './auth.component';
 import {LoginComponent} from './login/login.component';
 import {FormsModule} from '@angular/forms';
 import {LogoutComponent} from './logout/logout.component';
+import {RegisterComponent} from './register/register.component';
 
 @NgModule({
-    declarations: [AuthComponent, LoginComponent, LogoutComponent],
+    declarations: [AuthComponent, LoginComponent, LogoutComponent, RegisterComponent],
     imports: [
         CommonModule,
         ClarityModule,
