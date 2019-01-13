a1498c9e19a488d87f9dc8efc1b0908512900202
joostlek <joostlek@outlook.com>
2019-01-14 00:32:28 +0100

    Add creation and up-date to the DTO
    Signed-off-by: joostlek <joostlek@outlook.com>

--- backend/src/main/java/nl/jtosti/hermes/entities/dto/UserDTO.java
+++ backend/src/main/java/nl/jtosti/hermes/entities/dto/UserDTO.java
@@ -1,10 +1,14 @@
 package nl.jtosti.hermes.entities.dto;
 
+import java.util.Date;
+
 public class UserDTO {
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
+    private Date updated;
+    private Date created;
 
     public Long getId() {
         return id;
@@ -36,5 +40,21 @@ public class UserDTO {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
     }
 }
