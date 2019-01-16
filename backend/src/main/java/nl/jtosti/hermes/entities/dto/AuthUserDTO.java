127cb852502343cfff3eb39d11d94178823b7117
joostlek <joostlek@outlook.com>
2019-01-16 22:58:44 +0100

    Remove user from User Auth response
    Signed-off-by: joostlek <joostlek@outlook.com>

--- backend/src/main/java/nl/jtosti/hermes/entities/dto/AuthUserDTO.java
+++ backend/src/main/java/nl/jtosti/hermes/entities/dto/AuthUserDTO.java
@@ -1,7 +1,11 @@
 package nl.jtosti.hermes.entities.dto;
 
-public class AuthUserDTO extends UserDTO {
+public class AuthUserDTO {
     private String token;
+
+    public AuthUserDTO(String token) {
+        this.token = token;
+    }
 
     public String getToken() {
         return token;
