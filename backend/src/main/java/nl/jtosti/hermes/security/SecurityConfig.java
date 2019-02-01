14083f65c6b66abbce68835154b73754cf3a0b8e
joostlek <joostlek@outlook.com>
2019-02-01 16:35:53 +0100

    Fix register user returns forbidden
    Signed-off-by: joostlek <joostlek@outlook.com>

--- backend/src/main/java/nl/jtosti/hermes/security/SecurityConfig.java
+++ backend/src/main/java/nl/jtosti/hermes/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/auth/user/signin").permitAll()
+                .antMatchers("/auth/register").permitAll()
                 .antMatchers("/auth/screen/signin").permitAll()
                 .anyRequest().authenticated()
                 .and()
