5f5c8e8181fa2f07350c8e4d9c665c14b2aa6bed
joostlek <joostlek@outlook.com>
2019-02-05 15:58:58 +0100

    Try to fix travis
    Signed-off-by: joostlek <joostlek@outlook.com>

--- .travis/push-javadoc.sh
+++ .travis/push-javadoc.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "$TRAVIS_REPO_SLUG"
+echo "$TRAVIS_PULL_REQUEST"
+echo "$TRAVIS_BRANCH"
+
+
 if [ "$TRAVIS_REPO_SLUG" == "joostlek/Hermes" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo -e "Publishing javadoc...\n"
@@ -12,8 +17,8 @@ if [ "$TRAVIS_REPO_SLUG" == "joostlek/Hermes" ] && [ "$TRAVIS_PULL_REQUEST" == "
   git clone --quiet --branch=master https://${GH_TOKEN}@github.com/joostlek/joostlek.github.io gh-pages > /dev/null
 
   cd gh-pages
-  git rm -rf ./javadoc
-  cp -Rf $HOME/javadoc-latest ./javadoc
+  git rm -rf ./apidocs
+  cp -Rf $HOME/apidocs ./apidocs
   git add -f .
   git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
