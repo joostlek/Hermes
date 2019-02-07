49131767ca7d9d03ca986efffc4b454d6e7114cf
joostlek <joostlek@outlook.com>
2019-02-07 23:12:48 +0100

    Add basic image getting functionality
    Signed-off-by: joostlek <joostlek@outlook.com>

--- app/start.py
+++ app/start.py
@@ -1,1 +1,36 @@
-print('kek')
+import requests
+
+url = "http://localhost:8080"
+
+
+def run():
+    screen_id = 1
+    password = register(screen_id)
+    jwt = get_token(password, screen_id)
+    for image in get_images(jwt, screen_id):
+        print(image['name'])
+
+
+def register(screen_id):
+    res = requests.request('POST', url + '/auth/screen/register', json={'screenId': screen_id})
+    data = res.json()
+    print(data)
+    return data['password']
+
+
+def get_token(password, screen_id):
+    res = requests.post(url + '/auth/screen/signin', json={'screenId': screen_id, 'password': password})
+    data = res.json()
+    print(data)
+    return data['token']
+
+
+def get_images(jwt, screen_id):
+    res = requests.get(url + '/screens/' + str(screen_id) + '/images', headers={'Authorization': 'Bearer ' + jwt})
+    data = res.json()
+    print(data)
+    return data
+
+
+if __name__ == "__main__":
+    run()
