7ae49d78f73fe3f9bf64cda1aa5dc23d2fdfb38e
joostlek <joostlek@outlook.com>
2019-02-11 10:25:38 +0100

    Add buttons for editing and deleting
    Signed-off-by: joostlek <joostlek@outlook.com>

--- frontend/src/app/pages/location-dashboard/location-detail/location-detail.component.ts
+++ frontend/src/app/pages/location-dashboard/location-detail/location-detail.component.ts
@@ -1,68 +1,40 @@
 import {Component, OnInit} from '@angular/core';
 import {Location} from '../../../@core/data/domain/location';
-import {Screen} from '../../../@core/data/domain/screen';
-import {ActivatedRoute, Router} from '@angular/router';
-import {LocationService} from '../../../@core/data/location.service';
-import {ScreenService} from '../../../@core/data/screen.service';
-import {Location as NgLocation} from '@angular/common';
+import {ChosenLocationService} from '../chosen-location.service';
+import {Company} from '../../../@core/data/domain/company';
 
 @Component({
     selector: 'app-location-detail',
     templateUrl: './location-detail.component.html',
-    styleUrls: ['./location-detail.component.css']
+    styleUrls: ['./location-detail.component.css'],
 })
 export class LocationDetailComponent implements OnInit {
     location: Location;
-    screens: Screen[] = [];
-    informationActive = true;
-    screensActive = false;
+    company: Company;
 
     constructor(
-        private route: ActivatedRoute,
-        private router: Router,
-        private locationService: LocationService,
-        private screenService: ScreenService,
-        private ngLocation: NgLocation,
+        private chosenLocationService: ChosenLocationService,
     ) {
     }
 
     ngOnInit() {
-        this.getParameter();
+        this.getLocation();
+        this.getCompany();
     }
 
-    getParameter(): void {
-        this.route.params.subscribe(
-            (params) => {
-                if (params.hasOwnProperty('id')) {
-                    this.getLocation(+params['id']);
-                } else {
-                    this.getParentParameter();
-                }
-            },
-        );
-    }
-
-    getParentParameter(): void {
-        this.route.parent.parent.params.subscribe((params) => {
-            this.getLocation(+params['id']);
-        });
-    }
-
-    getLocation(id: number): void {
-        this.locationService.getLocationById(id)
+    private getLocation(): void {
+        this.chosenLocationService.getLocation()
             .subscribe((location) => {
-                this.location = location;
-                this.getScreens();
-            });
+                    this.location = location;
+                },
+            );
     }
 
-    getScreens(): void {
-        this.screenService.getScreensByLocation(this.location)
-            .subscribe((screens) => this.screens = screens);
+    private getCompany(): void {
+        this.chosenLocationService.getCompany()
+            .subscribe((company) => {
+                    this.company = company;
+                },
+            );
     }
-
-    goBack(): void {
-        this.ngLocation.back();
-    }
-
 }
