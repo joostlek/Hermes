cf4ff94b174cf7e3fb705123ca26c4e4cf395369
joostlek <joostlek@outlook.com>
2019-02-24 23:21:53 +0100

    Hide menu items for non-authorized users
    Signed-off-by: joostlek <joostlek@outlook.com>

--- frontend/src/app/pages/company-dashboard/company-dashboard.component.ts
+++ frontend/src/app/pages/company-dashboard/company-dashboard.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {BehaviorSubject} from 'rxjs';
 import {CompanyService} from '../../@core/data/company.service';
+import {CurrentUserService} from '../../@core/data/current-user.service';
 import {Company} from '../../@core/data/domain/company';
+import {User} from '../../@core/data/domain/user';
 import {ChosenCompanyService} from './chosen-company.service';
 
 @Component({
@@ -10,12 +13,15 @@ import {ChosenCompanyService} from './chosen-company.service';
     styleUrls: ['./company-dashboard.component.css'],
 })
 export class CompanyDashboardComponent implements OnInit {
+    admin: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
+    user: User;
     company: Company;
 
     constructor(
         private companyService: CompanyService,
         private chosenCompanyService: ChosenCompanyService,
         private route: ActivatedRoute,
+        private currentUserService: CurrentUserService,
     ) {
     }
 
@@ -37,8 +43,31 @@ export class CompanyDashboardComponent implements OnInit {
             .subscribe((company) => {
                     this.company = company;
                     this.chosenCompanyService.pushNewCompany(company);
+                this.getUser();
                 },
             );
+    }
+
+    private getUser(): void {
+        this.currentUserService.getCurrentUser()
+            .subscribe(
+                (user: User) => {
+                    this.user = user;
+                    this.checkRights();
+                },
+            );
+    }
+
+    public checkRights(): void {
+        let res = false;
+        this.company.users.forEach(
+            (user: User) => {
+                if (user.id === this.user.id) {
+                    res = true;
+                }
+            },
+        );
+        this.admin.next(res);
     }
 
 }
