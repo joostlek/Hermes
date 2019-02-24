338e98a9c9e6e7944fb5c4b4396d3b6ab862f727
joostlek <joostlek@outlook.com>
2019-02-24 22:59:59 +0100

    Add tabs for advertising and personal locations
    Signed-off-by: joostlek <joostlek@outlook.com>

--- frontend/src/app/pages/company-overview/company-overview.component.ts
+++ frontend/src/app/pages/company-overview/company-overview.component.ts
@@ -15,6 +15,8 @@ export class CompanyOverviewComponent implements OnInit {
 
     companyWizard: Subject<boolean> = new Subject<boolean>();
     allCompanies: BehaviorSubject<Company[]> = new BehaviorSubject(null);
+    personalCompanies: BehaviorSubject<Company[]> = new BehaviorSubject(null);
+    advertisingCompanies: BehaviorSubject<Company[]> = new BehaviorSubject(null);
 
     constructor(
         private currentUserService: CurrentUserService,
@@ -37,12 +39,34 @@ export class CompanyOverviewComponent implements OnInit {
                                 this.allCompanies.next(companies);
                             },
                         );
+                this.companyService.getPersonalCompanies(value.id)
+                    .subscribe((companies: Company[]) => {
+                            this.personalCompanies.next(companies);
+                        },
+                    );
+                this.companyService.getAdvertisingCompanies(value.id)
+                    .subscribe((companies: Company[]) => {
+                            this.advertisingCompanies.next(companies);
+                        },
+                    );
                 },
             );
     }
 
     public getAllCompanyStream(): Observable<Company[]> {
         return this.allCompanies.pipe(
+            filter((value) => value !== null),
+        );
+    }
+
+    public getPersonalCompanyStream(): Observable<Company[]> {
+        return this.personalCompanies.pipe(
+            filter((value) => value !== null),
+        );
+    }
+
+    public getAdvertisingCompanyStream(): Observable<Company[]> {
+        return this.advertisingCompanies.pipe(
             filter((value) => value !== null),
         );
     }
